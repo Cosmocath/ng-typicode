54961e18155884e0b380e3a59bdb11609ba26a1c
benoitsemifir <benoit.routier@semifir.com>
2021-09-01 17:07:01 +0200

    Checkpoint Mercredi soir: Aprés correction

--- src/app/article-page/article-page.component.ts
+++ src/app/article-page/article-page.component.ts
@@ -18,12 +18,12 @@ export class ArticlePageComponent implements OnInit {
   constructor(private route: ActivatedRoute, private articleService: ArticleService) { }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params.id
+    this.id = this.route.snapshot.params.id;
     this.getArticle(this.id);
   }
 
   private getArticle(id: number): void {
-    this.articleService.getArticle(this.id).subscribe((article: Article)=>{
+    this.articleService.getArticle(id).subscribe((article: Article)=>{
       this.article = article;
       this.getComments(this.article.id);
     });
